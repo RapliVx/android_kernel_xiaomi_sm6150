00de466162a125e0d20c1db305d3a1afbfc8ce06
EmanuelCN <emanuelghub@gmail.com>
2024-08-11 18:19:43 +0300

    Revert "sched/core: Set cpu_shares with uclamp assist"
    This reverts commit 9aec5e9ea2cba303b4a60ace4e8c4889ec0f2e50.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8552,28 +8552,22 @@ struct uclamp_param {
 	char uclamp_max[3];
 	u64  uclamp_latency_sensitive;
 	u64  uclamp_boosted;
-	u64  cpu_shares;
 };
-
-#ifdef CONFIG_FAIR_GROUP_SCHED
-static int cpu_shares_write_u64(struct cgroup_subsys_state *css,
-				struct cftype *cftype, u64 shareval);
-#endif
 
 static void uclamp_set(struct cgroup_subsys_state *css)
 {
 	int i;
 
 	static struct uclamp_param tgts[] = {
-		{"top-app",            "10",  "max",  1, 1, 20480},
-		{"rt",			"0",  "max",  1, 0, 20480},
-		{"nnapi-hal",		"0",  "max",  1, 0, 20480},
-       		{"foreground",          "0",  "max",  1, 0, 20480},
-                {"camera-daemon",      "10",  "max",  1, 1, 20480},
-                {"system",              "0",  "max",  0, 0, 20480},
-                {"dex2oat",             "0",  "60",   0, 0,   512},
-        	{"background",          "0",  "50",   0, 0,  1024},
-        	{"system-background",   "0",  "50",   0, 0,  1024},
+		{"top-app",            "10",  "max",  1, 1},
+		{"rt",			"0",  "max",  1, 0},
+		{"nnapi-hal",		"0",  "max",  1, 0},
+       		{"foreground",          "0",  "max",  1, 0},
+                {"camera-daemon",      "10",  "max",  1, 1},
+                {"system",              "0",  "max",  0, 0},
+                {"dex2oat",             "0",  "60",   0, 0},
+        	{"background",          "0",  "50",   0, 0},
+        	{"system-background",   "0",  "50",   0, 0},
 	};
 
         if(!css->cgroup->kn)
@@ -8591,21 +8585,11 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 						tgt.uclamp_latency_sensitive);
                         cpu_uclamp_boost_write_u64(css, NULL,
                                                 tgt.uclamp_boosted);
-#ifdef CONFIG_FAIR_GROUP_SCHED
-			cpu_shares_write_u64(css, NULL, tgt.cpu_shares);
-#endif
 
-#ifdef CONFIG_FAIR_GROUP_SCHED
-			pr_info("uclamp_assist: setting values for %s: uclamp_min=%s uclamp_max=%s"
-				"uclamp_latency_sensitive=%d uclamp_boosted=%d cpu_shares=%d\n",
-				tgt.name, tgt.uclamp_min, tgt.uclamp_max, tgt.uclamp_latency_sensitive,
-				tgt.uclamp_boosted, tgt.cpu_shares);
-#else
                         pr_info("uclamp_assist: setting values for %s: uclamp_min=%s uclamp_max=%s" 
 				"uclamp_latency_sensitive=%d\n uclamp_boosted=%d",
                                 tgt.name, tgt.uclamp_min, tgt.uclamp_max, tgt.uclamp_latency_sensitive,
 				tgt.uclamp_boosted);
-#endif
 
 			return;
 		}
